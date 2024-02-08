1fcea476536461f1e3c4f75e2e5f098b27749046
Aakash <aakashanand1995@gmail.com>
2024-02-08 18:49:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Landing from "./components/Landing.jsx";
+import { Navbar } from "./components/Navbar.jsx";
+
+function App() {
+  return (
+    <div className="text-white h-screen w-full">
+      <Navbar />
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Landing.jsx
@@ -0,0 +1,54 @@
+import { FaArrowUpLong } from "react-icons/fa6";
+
+function Landing() {
+  return (
+    <div className="w-full h-screen bg-zinc-900 pt-1">
+      <div className="textstructure mt-40 px-20 font-semibold">
+        {["We Create", "Eye-Opening", "Presentation"].map((element, index) => {
+          return (
+            <div key={index} className="masker overflow-hidden">
+              <div className="w-fit flex">
+                {index === 1 && (
+                  <img
+                    className="w-[8vw] h-[5vw] relative top-[1.8vh] rounded-lg mr-4"
+                    src={`https://ochi.design/wp-content/uploads/2022/04/content-image01.jpg`}
+                    alt="promotionArt"
+                  />
+                )}
+                <h1 className="uppercase text-[6vw] font-['Founders Grotesk X Cond-Lg'] leading-[6vw] tracking-tighter">
+                  {element}
+                </h1>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="border-t-[1px] border-zinc-800 mt-32 flex justify-between items-center pr-4">
+        {[
+          "For Public and Private Companies",
+          "From the First Pitch to IPO",
+        ].map((element, index) => {
+          return (
+            <p
+              key={index}
+              className="text-md font-light tracking-tight leading-none py-5 px-20"
+            >
+              {element}
+            </p>
+          );
+        })}
+        <div className="Start flex items-center gap-3">
+          <div className="px-5 py-2 border-[1px] border-zinc-500 font-light text-md uppercase rounded-full">
+            Start the Project
+          </div>
+          <div className="w-10 h-10 flex items-center justify-center border-[1px] rounded-full border-zinc-500">
+            <span className="rotate-[45deg]">
+              <FaArrowUpLong />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Landing;
