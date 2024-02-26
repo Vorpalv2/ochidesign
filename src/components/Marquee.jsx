2d1af7352b12e9565b04ab7727b480b7f6301cb6
Aakash <aakashanand1995@gmail.com>
2024-02-26 10:47:07 +0530

    added text scroll animation

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,14 +1,14 @@
 function Marquee() {
   return (
-    <div className="w-full h-[50vh] bg-[#004D43] py-10">
+    <div className="w-full h-[50vh] bg-[#004D43] py-10 ">
       <div className="text border-t-2 border-b-2 mt-[7vh] border-zinc-300 flex whitespace-nowrap gap-10 overflow-hidden">
-        <h1 className="text-[16vw] -tracking-[20px] leading-none font-['Founders_Grotesk_X-Condensed] font-semibold uppercase -mb-[10]">
+        <h1 className="animate-left-to-right text-[16vw] -tracking-[20px] leading-none font-['Founders_Grotesk_X-Condensed] font-semibold uppercase -mb-[10]">
           We are ochi
         </h1>
-        <h1 className="text-[16vw] -tracking-[20px] leading-none font-['Founders_Grotesk_X-Condensed] font-semibold uppercase -mb-[10]">
+        <h1 className="animate-left-to-right text-[16vw] -tracking-[20px] leading-none font-['Founders_Grotesk_X-Condensed] font-semibold uppercase -mb-[10]">
           We are ochi
         </h1>
-        <h1 className="text-[16vw] -tracking-[20px] leading-none font-['Founders_Grotesk_X-Condensed] font-semibold uppercase -mb-[10]">
+        <h1 className="animate-left-to-right text-[16vw] -tracking-[20px] leading-none font-['Founders_Grotesk_X-Condensed] font-semibold uppercase -mb-[10]">
           We are ochi
         </h1>
       </div>
