8fa6e351c2aadd632b1d47c8a7d31f3531bbaccc
Aakash <aakashanand1995@gmail.com>
2024-03-09 21:19:16 +0530

    minor improvements

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,18 +1,28 @@
+import { useEffect } from "react";
+
 const Eyes = () => {
+  useEffect(() => {
+    window.addEventListener("mousemove", (event) => {
+      let { clientX, clientY } = event;
+      console.log(clientX, clientY);
+    });
+  });
+
   return (
     <div className="eyes w-full h-screen overflow-hidden">
       <div className="relative w-full h-full bg-cover bg-center bg-[url(https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg)]">
         <div className="absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
           <div className="w-[15vw] flex items-center justify-center h-[15vw] rounded-full bg-zinc-100">
-            <div className="w-2/3 h-2/3 flex items-center justify-center rounded-full bg-zinc-900">
-              <div className="line w-full h-10 rotate-180">
+            <div className="relative w-2/3 h-2/3 rounded-full bg-zinc-900">
+              {/* <button className="absolute">PLAY</button> */}
+              <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] line w-full h-10 bg-red-400">
                 <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
               </div>
             </div>
           </div>
           <div className="w-[15vw] h-[15vw] flex items-center justify-center rounded-full bg-zinc-100">
-            <div className="w-2/3 h-2/3 rounded-full flex items-center justify-center bg-zinc-900">
-              <div className="line w-full h-10 rotate-15">
+            <div className="relative w-2/3 h-2/3 rounded-full flex items-center justify-center bg-zinc-900">
+              <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] line w-full h-10 bg-red-400">
                 <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
               </div>
             </div>
