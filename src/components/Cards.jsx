f320869d3da4263e25c119b1c748235d4fd50cb2
Aakash <aakashanand1995@gmail.com>
2024-03-10 18:41:25 +0530

    almost done with cards section

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,6 @@
 const Cards = () => {
   return (
-    <div className="w-full h-screen bg-zinc-900 flex gap-5 items-center px-32">
+    <div className="w-full h-screen bg-[#212121] rounded-lg flex gap-5 items-center px-32">
       <div className="cardcontainer h-[50vh] w-1/2">
         <div className="card relative flex items-center justify-center w-full rounded-xl h-full bg-[#004D43]">
           <img
@@ -13,22 +13,22 @@ const Cards = () => {
         </div>
       </div>
       <div className="relative cardcontainer flex gap-5 w-1/2 h-[50vh]">
-        <div className="card flex items-center justify-center rounded-xl w-full h-full bg-[#1e2d2c]">
+        <div className="card flex items-center justify-center rounded-xl w-full h-full bg-[#212121]">
           <img
             src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg"
             alt="clutch logo"
           />
-          <button className="absolute uppercase text-[#77A858] text-2xl left-10 bottom-10 px-5 py-1 rounded-full border-2">
+          <button className="absolute uppercase text-[#77A858] text-xl left-10 bottom-10 px-5 py-1 rounded-full border-2">
             rating 5.0 on clutch
           </button>
         </div>
-        <div className="relative card flex items-center justify-center rounded-xl w-full h-full bg-[#1e2d2c]">
+        <div className="relative card flex items-center justify-center rounded-xl w-full h-full bg-[#212121]">
           <img
-            src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg"
-            alt="clutch logo"
+            src="https://ochi.design/wp-content/uploads/2022/04/logo003.png"
+            alt="business bootcamp alumni"
           />
-          <button className="absolute text-[#77A858] text-2xl left-10 bottom-10 px-5 py-1 rounded-full border-2 uppercase">
-            Rating 5.0 on clutch
+          <button className="absolute text-[#77A858] text-xl left-10 bottom-10 px-5 py-1 rounded-full border-2 uppercase">
+            business bootcamp alumni
           </button>
         </div>
       </div>
