e54730e763993b6df832724f3296e852bacb3b04
Aakash <aakashanand1995@gmail.com>
2024-03-13 09:37:13 +0530

    speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -48,5 +48,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <SpeedInsights />
   </React.StrictMode>
 );
